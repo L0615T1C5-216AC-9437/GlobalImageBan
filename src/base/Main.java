8ceb60fc33202fae6a2a0db56bcddce258080e18
L0615T1C5-216AC-9437 <60281819+l0615t1c5-216ac-9437@users.noreply.github.com>
2021-12-06 08:16:04 -0500

    Bare Bone Skeleton

---
+++ src/base/Main.java
@@ -0,0 +1,22 @@
+package base;
+
+import arc.util.CommandHandler;
+import mindustry.mod.Plugin;
+
+public class Main extends Plugin {
+    public Main() {
+
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+
+    }
+
+
+}
