5097bb7b33f57a0c979a16c301b9126f9e81f4b5
LOGISTICAL NIGHTMARE <60281819+L0615T1C5-216AC-9437@users.noreply.github.com>
2021-12-09 10:13:55 -0500

    Revision 1.1

--- src/base/Main.java
+++ src/base/Main.java
@@ -9,6 +9,7 @@ import mindustry.game.EventType;
 import mindustry.gen.Groups;
 import mindustry.gen.Player;
 import mindustry.mod.Plugin;
+import mindustry.world.Tile;
 import mindustry.world.blocks.logic.LogicBlock;
 import org.json.JSONObject;
 
@@ -21,9 +22,32 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
 
 public class Main extends Plugin {
     private static MessageDigest messageDigest;
+    private static final HashMap<String, BMIData> cache = new HashMap<>();
+    private static class BMIData {
+        public final int httpResponse;
+        public final JSONObject data;
+        private final long expiration;
+
+        public BMIData(int httpResponse, JSONObject data) {
+            this.httpResponse = httpResponse;
+            this.data = data;
+            this.expiration = System.currentTimeMillis() + (24 * 60 * 60 * 1000L);
+        }
+
+        public boolean expired() {
+            return expiration < System.currentTimeMillis();
+        }
+
+        public boolean normalResponse() {
+            return httpResponse == 200 || httpResponse == 404;
+        }
+    }
+    private static long lastBroadcast = System.currentTimeMillis();
 
     public Main() {
         try {
@@ -35,48 +59,55 @@ public class Main extends Plugin {
 
         Events.on(EventType.BlockBuildEndEvent.class, event -> {
             if (event.breaking || event.unit == null || event.unit.getPlayer() == null) return;
-            Player player = event.unit.getPlayer();
+            final Player player = event.unit.getPlayer();
             if (event.tile.build instanceof LogicBlock.LogicBuild) {
-                LogicBlock.LogicBuild lb = (LogicBlock.LogicBuild) event.tile.build;
+                final LogicBlock.LogicBuild lb = (LogicBlock.LogicBuild) event.tile.build;
                 lb.configure(event.config);
                 //check if draws to display
                 if (lb.code.contains("drawflush display")) {
-                    String[] check = Core.settings.getBool("gib_complexSearch", false) ? lb.code.split("drawflush display.\n") : new String[]{ lb.code };
-                    for (String c : check) {
-                        try {
-                            byte[] hash = messageDigest.digest(c.getBytes(StandardCharsets.UTF_8));
-                            String b64Hash = Base64.getEncoder().encodeToString(hash);
-                            Log.debug(b64Hash);
-                            //http request
-                            URL url = new URL("http://c-n.ddns.net:9999/bmi/check/?b64hash=" + URLEncoder.encode(b64Hash, "UTF-8"));
-                            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                            con.setConnectTimeout(1000);
-                            con.setRequestMethod("GET");
-                            con.setDoOutput(true);
-                            con.setRequestProperty("Content-Type", "application/json");
-                            Log.debug(con.getResponseCode());
-                            if (con.getResponseCode() == 200) {
-                                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                                String inputLine;
-                                StringBuilder content = new StringBuilder();
-                                while ((inputLine = in.readLine()) != null) {
-                                    content.append(inputLine);
+                    CompletableFuture.runAsync(() -> {
+                        String[] check = Core.settings.getBool("gib_complexSearch", false) ? lb.code.split("drawflush display.\n") : new String[]{lb.code};
+                        for (String c : check) {
+                            try {
+                                byte[] hash = messageDigest.digest(c.getBytes(StandardCharsets.UTF_8));
+                                String b64Hash = Base64.getEncoder().encodeToString(hash);
+                                Log.debug(b64Hash);
+                                BMIData bmid = cache.get(b64Hash);
+                                if (bmid != null && !bmid.expired() && bmid.normalResponse()) {
+                                    Log.debug("b64hash found in cache.");
+                                    if (bmid.httpResponse == 200) handleHit(player, event.tile, bmid.data);
+                                } else {
+                                    Log.debug("b64hash not found in cache, performing http get.");
+                                    //http request
+                                    URL url = new URL("http://c-n.ddns.net:9999/bmi/check/?b64hash=" + URLEncoder.encode(b64Hash, "UTF-8"));
+                                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                                    con.setConnectTimeout(Core.settings.getInt("gib_ConnectionTimeout", 1000));
+                                    con.setRequestMethod("GET");
+                                    con.setDoOutput(true);
+                                    con.setRequestProperty("Content-Type", "application/json");
+                                    int httpResponse = con.getResponseCode();
+                                    Log.debug(httpResponse);
+                                    if (httpResponse == 200) {
+                                        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                                        String inputLine;
+                                        StringBuilder content = new StringBuilder();
+                                        while ((inputLine = in.readLine()) != null) {
+                                            content.append(inputLine);
+                                        }
+                                        in.close();
+                                        JSONObject json = new JSONObject(content.toString());
+                                        cache.put(b64Hash, new BMIData(httpResponse, json));
+                                        handleHit(player, event.tile, json);
+                                        break; //stop checking if hit
+                                    } else {
+                                        cache.put(b64Hash, new BMIData(httpResponse, new JSONObject()));
+                                    }
                                 }
-                                in.close();
-                                JSONObject json = new JSONObject(content.toString());
-                                if (Core.settings.getBool("gib_kickOnHit", false)) {
-                                    player.con.kick("[scarlet]Built banned logic image\n[lightgray]ID: " + json.get("id") + "\nBID: " + json.get("bid"), 3 * 60 * 60 * 1000L);
-                                }
-                                //send message to everyone but the player building it
-                                for (Player p : Groups.player) {
-                                    if (p != player) p.sendMessage("[scarlet]> " + colorless(player.name) + " [scarlet]built banned image (prob NSFW) @ (" + event.tile.x + "," + event.tile.y + ")\n[gray]ID:" + json.get("id") + " | bid:" + json.get("bid"));
-                                }
-                                break; //stop checking if hit
+                            } catch (Exception e) {
+                                Log.err(Strings.neatError(e));
                             }
-                        } catch (Exception e) {
-                            Log.err(Strings.neatError(e));
                         }
-                    }
+                    });
                 }
             }
         });
@@ -93,11 +124,41 @@ public class Main extends Plugin {
             Log.info("gib_kickOnHit set to " + Core.settings.getBool("gib_kickOnHit", false));
 
         });
+        handler.register("gibcbt", "<milliseconds>", "Configure Broadcast Timeout", args -> {
+            if (Strings.canParseInt(args[0])) {
+                Core.settings.put("gib_BroadcastTimeout", Strings.parseInt(args[0]));
+                System.out.println("Broadcast Timeout set to " + args[0].trim() + "ms");
+            } else {
+                System.out.println("Must be a number!");
+            }
+        });
+        handler.register("gibcct", "<milliseconds>", "Configure Connection Time Out", args -> {
+            if (Strings.canParseInt(args[0])) {
+                Core.settings.put("gib_ConnectionTimeout", Strings.parseInt(args[0]));
+                System.out.println("Connection Timeout set to " + args[0].trim() + "ms");
+            } else {
+                System.out.println("Must be a number!");
+            }
+        });
     }
 
     @Override
     public void registerClientCommands(CommandHandler handler) {
 
+    }
+
+    private static void handleHit(Player player, Tile t, JSONObject json) {
+        if (Core.settings.getBool("gib_kickOnHit", false)) {
+            player.con.kick("[scarlet]Built banned logic image\n[lightgray]ID: " + json.get("id") + "\nBID: " + json.get("bid"), 3 * 60 * 60 * 1000L);
+        }
+        if (lastBroadcast < System.currentTimeMillis()) {
+            lastBroadcast = System.currentTimeMillis() + Core.settings.getInt("gib_BroadcastTimeout", 2000);
+            //send message to everyone but the player building it
+            for (Player p : Groups.player) {
+                if (p != player)
+                    p.sendMessage("[scarlet]> " + colorless(player.name) + " [scarlet]built banned image (prob NSFW) @ (" + t.x + "," + t.y + ")\n[gray]ID:" + json.get("id") + " | bid:" + json.get("bid"));
+            }
+        }
     }
 
     public static String colorless(String string) {
